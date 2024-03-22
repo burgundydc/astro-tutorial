5c8f4375204e0bab6106a6ff2eacb89eb180222c
mjbowyer <mjbowyer@gmail.com>
2024-03-22 19:18:21 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()],
+  site: 'https://astrotutorial45464674768.netlify.app',
+  integrations: [alpinejs(), sitemap()],
   output: "server",
   adapter: netlify()
 });
