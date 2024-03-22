05417fdd924c67501daeeb96255f3ae34cf75c05
mjbowyer <mjbowyer@gmail.com>
2024-03-22 19:10:02 -0400

    added form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import alpinejs from "@astrojs/alpinejs";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs()],
+  output: "server",
+  adapter: netlify()
+});
